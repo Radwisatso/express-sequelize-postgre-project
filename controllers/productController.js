cde43d4956a9966d1a6b9a151c742714b79b77b1
Raditya Dwisatrio Santoso <dwisatrioraditya@gmail.com>
2023-02-03 13:41:02 +0700

    Finishing add product

--- controllers/productController.js
+++ controllers/productController.js
@@ -15,6 +15,56 @@ class ProductController {
             })
     }
 
+    static getOneData(req, res) {
+        Product.findOne({
+            where: {
+                id: +req.params.id
+            },
+            include: [Dealer]
+        })
+            .then(result => {
+                res.status(200).json(result)
+            })
+            .catch(err => {
+                res.status(500).json(err)
+            })
+    }
+
+    static readCreateData(req, res) {
+        Dealer.findAll()
+            .then(result => {
+                res.render('addProduct', { dealers: result })
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    static createData(req, res) {
+        const { name, stock, DealerId } = req.body
+        console.log(name, stock, DealerId)
+        Product.create({
+            name,
+            stock,
+            DealerId
+        })
+        .then(result => {
+            console.log(result, 'ini result ================')
+            res.redirect('/products')
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    static updateData(req, res) {
+
+    }
+
+    static deleteData(req, res) {
+        
+    }
+
 }
 
 module.exports = ProductController;
--- routers/productRoute.js
+++ routers/productRoute.js
@@ -3,5 +3,8 @@ const ProductController = require('../controllers/productController');
 const router = require('express').Router()
 
 router.get('/', ProductController.getData)
+router.get('/add', ProductController.readCreateData)
+router.get('/:id', ProductController.getOneData)
+router.post('/add', ProductController.createData)
 
 module.exports = router;
